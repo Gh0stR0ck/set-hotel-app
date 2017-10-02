1d012c0d755fdf0b9e40fb67e614db12b9c0fb81
akekim <akin.ekim@capgemini.com>
2017-10-02 14:37:10 +0200

    database + roommanager

--- src/main/java/com/capgemini/hotel/App.java
+++ src/main/java/com/capgemini/hotel/App.java
@@ -2,16 +2,32 @@ package com.capgemini.hotel;
 
 import com.capgemini.hotel.model.Guest;
 import com.capgemini.hotel.model.Hotel;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.capgemini.hotel.utils.Database;
+import java.sql.ResultSet;
 
-@SpringBootApplication
+
 public class App {
 
     public static Hotel hotel = new Hotel();
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(App.class, args);
+        System.out.println("Database");
+        Database db = new Database(
+                "capgemini-academy.ck17qz3qdemp.eu-west-1.rds.amazonaws.com",
+                "hotel1",
+                "capgemini",
+                "hotel1");
 
+        db.open();
+
+        ResultSet resultSet = db.query("SELECT * FROM Room;");
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getInt("roomNr"));
+        }
+
+        resultSet.close();
+
+        db.close();
     }
 }
