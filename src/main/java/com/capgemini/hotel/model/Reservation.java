80b4a2f80a83eeeec5f8a2a76598d101a238ffcb
Victor <Victor.Tenbergen@capgemini.com>
2017-10-04 14:40:24 +0200

    Kleine correctie

--- src/main/java/com/capgemini/hotel/model/Reservation.java
+++ src/main/java/com/capgemini/hotel/model/Reservation.java
@@ -10,9 +10,51 @@ public class Reservation {
     private LocalDateTime endDate;
     private boolean payment;
 
+    public Reservation(Guest guest, Room room, LocalDateTime startDate, LocalDateTime endDate, boolean payment) {
+        this.guest = guest;
+        this.room = room;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.payment = payment;
+    }
+
+    public Guest getGuest() {
+        return guest;
+    }
+
+    public void setGuest(Guest guest) {
+        this.guest = guest;
+    }
+
     public Room getRoom() {
         return room;
     }
 
+    public void setRoom(Room room) {
+        this.room = room;
+    }
 
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isPayment() {
+        return payment;
+    }
+
+    public void setPayment(boolean payment) {
+        this.payment = payment;
+    }
 }
