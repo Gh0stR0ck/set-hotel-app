aaa9a89c9af05cf766bd377d267ad87b0a319868
dburger <mail@duncanburger.nl>
2017-10-04 09:39:45 +0200

    Add guest functionality

--- src/main/java/com/capgemini/hotel/controller/roomController.java
+++ src/main/java/com/capgemini/hotel/controller/roomController.java
@@ -1,28 +1,28 @@
-package com.capgemini.hotel.controller;
-
-import com.capgemini.hotel.model.Room;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
-
-import static com.capgemini.hotel.App.hotel;
-
-public class roomController {
-    @RequestMapping("/api/Rooms")
-    @ResponseBody
-    public List<Room> roomList() {
-
-        Room room1 = new Room(null, null, null, null);
-        Room room2 = new Room(null, null, null, null);
-        Room room3 = new Room(null, null, null, null);
-        hotel.roomManager.addRoom(room1);
-        hotel.roomManager.addRoom(room2);
-        hotel.roomManager.addRoom(room3);
-
-        return hotel.roomManager.getRoomList();
-
-
-    }
-}
-}
+//package com.capgemini.hotel.controller;
+//
+//import com.capgemini.hotel.model.Room;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import java.util.List;
+//
+//import static com.capgemini.hotel.App.hotel;
+//
+//public class roomController {
+//    @RequestMapping("/api/Rooms")
+//    @ResponseBody
+//    public void roomList() {
+//
+////        Room room1 = new Room(null, null, null, null);
+////        Room room2 = new Room(null, null, null, null);
+////        Room room3 = new Room(null, null, null, null);
+////        hotel.roomManager.addRoom(room1);
+////        hotel.roomManager.addRoom(room2);
+////        hotel.roomManager.addRoom(room3);
+////
+////        return hotel.roomManager.getRoomList();
+//
+//
+//    }
+//}
+//}
